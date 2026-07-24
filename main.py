0ea2538b5d4e4514f307c6e115513c73f932efd0
Muhammadjon <muhammadjondoltaboyev@gmail.com>
2026-07-24 23:54:25 +0500

    Router qoshilgani

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from pydantic import BaseModel
 
 app = FastAPI()
+router = APIRouter()
+
+class UserCreate(BaseModel):
+    name: str
+    address: str
+    phone: str
+
+class UserUpdate(BaseModel):
+    name: str
+    address: str
+    phone: str
+
 
 users = [
     {"id": 1, "name": "Toxir Toxirov", "address": "Fergana", "phone": "+99890"},
@@ -10,18 +23,18 @@ users = [
     {"id": 5, "name": "Bakir Sobirov", "address": "Namangan", "phone": "+99877"}
 ]
 
-@app.get('/users')
+@router.get('/users')
 def get_users():
     return users
 
-@app.get('/users/{user_id}')
+@router.get('/users/{user_id}')
 def get_user(user_id: int):
     for user in users:
         if user["id"] == user_id:
             return user
     return {"xatolik": "Bunaqa foydalanuvchi topilmadi!!!"}
 
-@app.get('/users/address/{address}')
+@router.get('/users/address/{address}')
 def get_user_by_address(address: str):
     result = []
     for user in users:
@@ -31,14 +44,34 @@ def get_user_by_address(address: str):
         return {"xatolik": "Bu manzilda user yo'q"}
     return result
 
-@app.post('/users')
-def add_user(name: str, address: str, phone: str):
+@router.post('/users')
+def add_user(user: UserCreate):
     new_id = len(users) + 1
     new_user = {
         "id": new_id,
-        "name": name,
-        "address": address,
-        "phone": phone
+        "name": user.name,
+        "address": user.address,
+        "phone": user.phone
     }
     users.append(new_user)
     return {"message": "Yangi foydalanuvchi qo'shildi", "user": new_user}
+
+@router.put('/users/{user_id}')
+def update_user(user_id: int, user: UserUpdate):
+    for u in users:
+        if u["id"] == user_id:
+            u["name"] = user.name
+            u["address"] = user.address
+            u["phone"] = user.phone
+            return {"message": "Foydalanuvchi yangilandi", "user": u}
+    return {"xatolik": "Bunaqa foydalanuvchi topilmadi!!!"}
+
+@router.delete('/users/{user_id}')
+def delete_user(user_id: int):
+    for i in range(len(users)):
+        if users[i]["id"] == user_id:
+            deleted = users.pop(i)
+            return {"message": "Foydalanuvchi o'chirildi", "user": deleted}
+    return {"xatolik": "Bunaqa foydalanuvchi topilmadi!!!"}
+
+app.include_router(router)
